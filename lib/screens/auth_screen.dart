03c104e9e49e3e168e65ccf1405fafe975bbfd46
RichardDanquah <richpee60@yahoo.com>
2021-06-03 14:23:53 +0000

    firebase added

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../widgets/auth/auth_form.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  final _auth = FirebaseAuth.instance;
+  //final _auth = FirebaseAuth.instance;
 
   void _submitAuthForm(String name,
       int number,
@@ -19,9 +18,9 @@ class _AuthScreenState extends State<AuthScreen> {
     var authResult;
     try{
       if(isLogin){
-        authResult = await _auth.signInWithEmailAndPassword(email: email, password: password);
+        //authResult = await _auth.signInWithEmailAndPassword(email: email, password: password);
       }else{
-        authResult = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+        //authResult = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       }
     } on PlatformException catch(err){
       var message = 'An error occurred. Please check your credentials';
